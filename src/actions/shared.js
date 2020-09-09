60fce3f86af6dc49c4286139a20be3cdbf302430
jamiekim828 <jamiekim828@gmail.com>
2020-09-09 17:40:25 +0200

    mapStateToProps

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,10 +7,26 @@ const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return (dispatch) => {
-    return getInirialData().then(({ users, tweets }) => {
+    return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
-      dispatch(setAuthedUser(AUTHED_ID)
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
+
+// action creator will go through thunk middleware
+
+// function createThunkMiddleware(extraArgument) {
+//   return ({ dispatch, getState }) => next => action => {
+//     if (typeof action === 'function') {
+//       return action(dispatch, getState, extraArgument);
+//     }
+//     return next(action);
+//   };
+// }
+
+// const thunk = createThunkMiddleware();
+// thunk.withExtraArgument = createThunkMiddleware;
+
+// export default thunk;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
+    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
   }
 }
 
-export default App
+// mapStateToProps(state, [ownProps])
+// ex.
+// function mapStateToProps( {tweets} ){
+//   return { tweetIds: Object.keys(tweets) };
+// }
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+// Using the connect() function upgrades a component to a container
+// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
+export default connect()(App);
